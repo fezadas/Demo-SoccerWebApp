8d5ca6a649174d93f55636b8f6e7af89a8c41ff0
fezadas <filipe.machado.fe@gmail.com>
2018-07-10 15:43:35 +0100

    Trying to use my webApp

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const pg        = require('pg');
 const express   = require('express');
 const app       = express();
+const PORT = process.env.PORT;
 
 const config = {
+    host:'ec2-54-235-66-81.compute-1.amazonaws.com',
     user: 'yaqralrajdjqyz',
-    database: 'yaqralrajdjqyz',
+    database: 'd3d5jccv5mvkm1',
     password: 'ee05bdd77d2a0f664c504fba57e77fee496731670abbfe1f8bf87aff5e08a2f7',
-    port: 5432
+    port: 5432,
+    ssl: true
 };
 
 // pool takes the object above -config- as parameter
@@ -28,6 +31,9 @@ app.get('/', (req, res, next) => {
    })
 });
 
-app.listen(4000, function () {
-    console.log('Server is running.. on Port 4000');
+app.listen(PORT || 4000, function () {
+    var port
+    if(!PORT) port=4000;
+    else port = PORT
+    console.log('Server is running.. on Port '+port);
 });
